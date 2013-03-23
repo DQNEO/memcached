76f01c8f8fd79f658b47a379cab5b03c33014384
DQNEOO <DQNEOO@gmail.com>
2013-03-23 18:54:52 +0000

    refactor

--- autogen.sh
+++ autogen.sh
@@ -8,18 +8,18 @@
 #
 
 echo "aclocal..."
-ACLOCAL=${ACLOCAL:-aclocal-1.7}
+ACLOCAL=aclocal-1.7
 $ACLOCAL || aclocal-1.5 || aclocal || exit 1
 
 echo "autoheader..."
-AUTOHEADER=${AUTOHEADER:-autoheader}
+AUTOHEADER=autoheader
 $AUTOHEADER || exit 1
 
 echo "automake..."
-AUTOMAKE=${AUTOMAKE:-automake-1.7}
+AUTOMAKE=automake-1.7
 $AUTOMAKE --foreign --add-missing || automake --gnu --add-missing || exit 1
 
 echo "autoconf..."
-AUTOCONF=${AUTOCONF:-autoconf}
+AUTOCONF=autoconf
 $AUTOCONF || exit 1
 
