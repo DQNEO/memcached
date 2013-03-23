e5dd6b5567c7fc120d77ddd592a5346dd37bcfe9
DQNEOO <DQNEOO@gmail.com>
2013-03-23 21:02:16 +0000

    remove UDP

--- memcached.c
+++ memcached.c
@@ -482,39 +482,10 @@ int add_iov(conn *c, const void *buf, int len) {
 
 /*
  * Constructs a set of UDP headers and attaches them to the outgoing messages.
+ * (DEPRECATED)
  */
 int build_udp_headers(conn *c) {
-    int i;
-    unsigned char *hdr;
-
-    if (c->msgused > c->hdrsize) {
-        void *new_hdrbuf;
-        if (c->hdrbuf)
-            new_hdrbuf = realloc(c->hdrbuf, c->msgused * 2 * UDP_HEADER_SIZE);
-        else
-            new_hdrbuf = malloc(c->msgused * 2 * UDP_HEADER_SIZE);
-        if (! new_hdrbuf)
-            return -1;
-        c->hdrbuf = (unsigned char *) new_hdrbuf;
-        c->hdrsize = c->msgused * 2;
-    }
-
-    hdr = c->hdrbuf;
-    for (i = 0; i < c->msgused; i++) {
-        c->msglist[i].msg_iov[0].iov_base = hdr;
-        c->msglist[i].msg_iov[0].iov_len = UDP_HEADER_SIZE;
-        *hdr++ = c->request_id / 256;
-        *hdr++ = c->request_id % 256;
-        *hdr++ = i / 256;
-        *hdr++ = i % 256;
-        *hdr++ = c->msgused / 256;
-        *hdr++ = c->msgused % 256;
-        *hdr++ = 0;
-        *hdr++ = 0;
-        assert((void*) hdr == (void*) c->msglist[i].msg_iov[0].iov_base + UDP_HEADER_SIZE);
-    }
-
-    return 0;
+    return -1;
 }
 
 
